3a7ea6c86d6129adc8aa1fa5c7573548a41bfb36
jwalterskirchen <82029355+jwalterskirchen@users.noreply.github.com>
2024-06-20 09:44:03 +0200

    Update priogrid-month-predictions.py

--- pages/priogrid-month-predictions.py
+++ pages/priogrid-month-predictions.py
@@ -5,7 +5,7 @@ import plotly.express as px
 from datetime import datetime
 import geopandas as gpd
 
-st.set_page_config(layout="wide", page_title="TFT - Prediction Competition", page_icon="dove_of_peace")
+st.set_page_config(layout="wide", page_title="TFT - Prediction Competition", page_icon="dove_of_peace", initial_sidebar_state="collapsed")
 
 logo_link = 'kompzkfe_logo.png'
 with st.sidebar:
