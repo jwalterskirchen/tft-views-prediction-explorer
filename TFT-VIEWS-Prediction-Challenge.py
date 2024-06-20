d27dc894f77cccb48f9c4c07ab1dc52654a00a6d
jwalterskirchen <82029355+jwalterskirchen@users.noreply.github.com>
2024-06-20 09:19:50 +0200

    Add files via upload

---
+++ TFT-VIEWS-Prediction-Challenge.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(layout="wide", page_title="TFT - Prediction Competition", page_icon="dove_of_peace")
+
+logo_link = 'kompzkfe_logo.png'
+with st.sidebar:
+    st.logo(logo_link, link="https://www.unibw.de/ciss-en/kompz-kfe/")
+    #st.markdown("[![KompZ KFE](app/static/kompzkfe_logo.png)](https://www.unibw.de/ciss-en/kompz-kfe/)", unsafe_allow_html=True)
+    #st.title("Sections")
+    #st.page_link("pages/country-month-predictions.py", label="Country Month Predictions")
+    #st.page_link("pages/priogrid-month-predictions.py", label="PRIO-Grid Month Predictions")
+
+
+st.title('Taking time seriously: Predicting conflict fatalities using temporal fusion transformers')
+
+st.markdown(" **Authors**: <a href='https://jwalterskirchen.github.io/' style='text-decoration: none; color: black; font-weight: bold;'>Julian Walterskirchen</a> and <a href='https://www.unibw.de/ciss-en/kompz-kfe/team/sonja-haffner-m-sc' style='text-decoration: none; color: black; font-weight: bold;'>Sonja Häffner</a> and <a href='https://www.unibw.de/ciss/kompzkfe/personen/christian-oswald-m-a-1' style='text-decoration: none; color: black; font-weight: bold;'>Christian Oswald</a> and <a href='https://www.uni-bremen.de/en/institut-fuer-interkulturelle-und-internationale-studien/personen/wissenschaftliche-mitarbeiterinnen/marco-nicola-binetti' style='text-decoration: none; color: black; font-weight: bold;'>Marco N. Binetti</a>", unsafe_allow_html=True)
+
+st.markdown(f'''
+**Abstract:**
+Previous conflict forecasting efforts identified three areas for improvement: the importance of spatiotemporal dependencies and nonlinearities and the need 
+to exploit the latent information contained in conflict variables further, a lack of interpretability in return for high accuracy of complex algorithms, and 
+the need to quantify uncertainty around point forecasts. We predict fatalities with temporal fusion transformer models which have several desirable features 
+for conflict forecasting and tackle all these points. First, they can produce multi-horizon forecasts and probabilistic predictions through quantile regression. 
+This offers a flexible and non-parametric approach to estimate prediction uncertainty. Second, they can incorporate time-invariant covariates, known future inputs,
+ and other exogenous time series which allows to identify globally important variables, persistent temporal patterns, and significant events for our prediction 
+ problem. This mechanism makes them suitable to model both long-term and short-term dependencies. Third, this approach puts a strong focus on interpretability 
+ such that we can investigate temporal dynamics more thoroughly via temporal self-attention decoders. Our models outperform an award-winning early warning
+  system on several metrics and test windows and are thus a valuable addition to the forecaster's toolkit.
+''')
+
+col1, col2 = st.columns(2, gap='small')
+
+with col1:
+    st.subheader('CRPS overview - Country-Month')
+    st.image('CRPS_overview.jpg')
+
+with col2:
+    st.subheader('CRPS overview - PRIOGrid-Month')
+    st.image('CRPS_overview_pgm.jpg')
+
+
