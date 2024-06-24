40d8c54265357fdbb06ff63ae67c2b3eb8e5cc18
jwalterskirchen <82029355+jwalterskirchen@users.noreply.github.com>
2024-06-24 14:47:43 +0200

    Update priogrid-month-predictions.py

--- pages/priogrid-month-predictions.py
+++ pages/priogrid-month-predictions.py
@@ -15,9 +15,10 @@ with st.sidebar:
 st.sidebar.header("PRIOGrid-month Predictions")
 
 st.title('Taking time seriously: Predicting conflict fatalities using temporal fusion transformers')
-st.header('PRIOGrid Historical Forecast and True Future Predictions')
-
 st.markdown(" **Authors**: <a href='https://jwalterskirchen.github.io/' style='text-decoration: none; color: black; font-weight: bold;'>Julian Walterskirchen</a> and <a href='https://www.unibw.de/ciss-en/kompz-kfe' style='text-decoration: none; color: black; font-weight: bold;'>Sonja Häffner</a> and <a href='https://www.unibw.de/ciss/kompzkfe/personen/christian-oswald-m-a-1' style='text-decoration: none; color: black; font-weight: bold;'>Christian Oswald</a> and <a href='https://www.uni-bremen.de/en/institut-fuer-interkulturelle-und-internationale-studien/personen/wissenschaftliche-mitarbeiterinnen/marco-nicola-binetti' style='text-decoration: none; color: black; font-weight: bold;'>Marco N. Binetti</a>", unsafe_allow_html=True)
+
+
+st.header('PRIOGrid Historical Forecast and True Future Predictions')
 
 tab1, tab2 = st.tabs(["Historical Forecast", "True Future Forecast"])
 
