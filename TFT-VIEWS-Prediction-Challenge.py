2a0efa879ab4074a67d8746bf749c04fc400fa66
jwalterskirchen <82029355+jwalterskirchen@users.noreply.github.com>
2024-06-25 10:58:53 +0200

    Update TFT-VIEWS-Prediction-Challenge.py

--- TFT-VIEWS-Prediction-Challenge.py
+++ TFT-VIEWS-Prediction-Challenge.py
@@ -22,8 +22,8 @@ st.markdown('''
 On this website, you can explore the Temporal Fusion Transformer (TFT) predictions for the historical test windows (2018-2023) and the true future forecast (July 2024-June 2025)
 that we submitted to the VIEWS Prediction Challenge (https://viewsforecasting.org/research/prediction-challenge-2023) in more detail. We provide
 evaluation metrics (CRPS, IGN, MIS) for all historical forecasts at both the country-month level and the PRIOGrid-level. Furthermore, model performance can be compared to the results
-achieved by several VIEWS benchmark models. For the true future forecasts, users can investigate our probabilistic predictions by adjusting the prediction interval level to obtain lower and upper
-bound predictions.
+achieved by several VIEWS benchmark models. For the true future forecasts, users can investigate our probabilistic predictions by adjusting the prediction interval level to obtain lower- and upper-bound predictions.
+Further details about the models, benchmarks, and evaluation metrics can be found in our preprint article at XYZ.com.
 ''')
 
 st.markdown(f'''
