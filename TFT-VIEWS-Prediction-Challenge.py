c8262a59afa2fda0defcc3a342d9c2bc86324c2a
jwalterskirchen <82029355+jwalterskirchen@users.noreply.github.com>
2024-06-25 11:06:32 +0200

    Update TFT-VIEWS-Prediction-Challenge.py

--- TFT-VIEWS-Prediction-Challenge.py
+++ TFT-VIEWS-Prediction-Challenge.py
@@ -18,28 +18,28 @@ st.title('Taking time seriously: Predicting conflict fatalities using temporal f
 
 st.markdown(" **Authors**: <a href='https://jwalterskirchen.github.io/' style='text-decoration: none; color: black; font-weight: bold;'>Julian Walterskirchen</a> and <a href='https://www.unibw.de/ciss-en/kompz-kfe' style='text-decoration: none; color: black; font-weight: bold;'>Sonja Häffner</a> and <a href='https://www.unibw.de/ciss/kompzkfe/personen/christian-oswald-m-a-1' style='text-decoration: none; color: black; font-weight: bold;'>Christian Oswald</a> and <a href='https://www.uni-bremen.de/en/institut-fuer-interkulturelle-und-internationale-studien/personen/wissenschaftliche-mitarbeiterinnen/marco-nicola-binetti' style='text-decoration: none; color: black; font-weight: bold;'>Marco N. Binetti</a>", unsafe_allow_html=True)
 
-with st.expander("VIEWS Prediction Challenge 2023/24"):
-   st.markdown('''
-        On this website, you can explore the Temporal Fusion Transformer (TFT) predictions for the historical test windows (2018-2023) and the true future forecast (July 2024-June 2025)
-        that we submitted to the VIEWS Prediction Challenge (https://viewsforecasting.org/research/prediction-challenge-2023) in more detail. We provide
-        evaluation metrics (CRPS, IGN, MIS) for all historical forecasts at both the country-month level and the PRIOGrid-level. Furthermore, model performance can be compared to the results
-        achieved by several VIEWS benchmark models. For the true future forecasts, users can investigate our probabilistic predictions by adjusting the prediction interval level to obtain lower- and upper-bound predictions.
-        Further details about the models, benchmarks, and evaluation metrics can be found in our preprint article at XYZ.com.
+
+st.markdown('''
+     On this website, you can explore the Temporal Fusion Transformer (TFT) predictions for the historical test windows (2018-2023) and the true future forecast (July 2024-June 2025)
+    that we submitted to the VIEWS Prediction Challenge (https://viewsforecasting.org/research/prediction-challenge-2023) in more detail. We provide
+    evaluation metrics (CRPS, IGN, MIS) for all historical forecasts at both the country-month level and the PRIOGrid-level. Furthermore, model performance can be compared to the results
+    achieved by several VIEWS benchmark models. For the true future forecasts, users can investigate our probabilistic predictions by adjusting the prediction interval level to obtain lower- and upper-bound predictions.
+    Further details about the models, benchmarks, and evaluation metrics can be found in our preprint article at XYZ.com.
+    ''')
+
+with st.expander("Abstract"):
+    st.markdown(f'''
+        **Abstract:**
+        Previous conflict forecasting efforts identified three areas for improvement: the importance of spatiotemporal dependencies and nonlinearities and the need 
+        to exploit the latent information contained in conflict variables further, a lack of interpretability in return for high accuracy of complex algorithms, and 
+        the need to quantify uncertainty around point forecasts. We predict fatalities with temporal fusion transformer models which have several desirable features 
+        for conflict forecasting and tackle all these points. First, they can produce multi-horizon forecasts and probabilistic predictions through quantile regression. 
+        This offers a flexible and non-parametric approach to estimate prediction uncertainty. Second, they can incorporate time-invariant covariates, known future inputs,
+        and other exogenous time series which allows to identify globally important variables, persistent temporal patterns, and significant events for our prediction 
+        problem. This mechanism makes them suitable to model both long-term and short-term dependencies. Third, this approach puts a strong focus on interpretability 
+        such that we can investigate temporal dynamics more thoroughly via temporal self-attention decoders. Our models outperform an award-winning early warning
+        system on several metrics and test windows and are thus a valuable addition to the forecaster's toolkit.
         ''')
-
-
-st.markdown(f'''
-**Abstract:**
-Previous conflict forecasting efforts identified three areas for improvement: the importance of spatiotemporal dependencies and nonlinearities and the need 
-to exploit the latent information contained in conflict variables further, a lack of interpretability in return for high accuracy of complex algorithms, and 
-the need to quantify uncertainty around point forecasts. We predict fatalities with temporal fusion transformer models which have several desirable features 
-for conflict forecasting and tackle all these points. First, they can produce multi-horizon forecasts and probabilistic predictions through quantile regression. 
-This offers a flexible and non-parametric approach to estimate prediction uncertainty. Second, they can incorporate time-invariant covariates, known future inputs,
- and other exogenous time series which allows to identify globally important variables, persistent temporal patterns, and significant events for our prediction 
- problem. This mechanism makes them suitable to model both long-term and short-term dependencies. Third, this approach puts a strong focus on interpretability 
- such that we can investigate temporal dynamics more thoroughly via temporal self-attention decoders. Our models outperform an award-winning early warning
-  system on several metrics and test windows and are thus a valuable addition to the forecaster's toolkit.
-''')
 
 with st.expander("VIEWS Prediction Challenge 2023/24"):
     st.write('''
